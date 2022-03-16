9bec18ebe8c8aa0b2793b69a65059ea90cbbfea1
Nancy Yeh <nancy@learnwithjuni.com>
2022-03-16 01:44:10 -0700

    fix heroku error

--- server.js
+++ server.js
@@ -2,6 +2,11 @@ const app = require("./app");
 
 // Started Server
 const port = process.env.PORT || 3000;
-app.listen(port, () =>
+let server = app.listen(port, () =>
   console.log(`You have been connected to the port number:${port}`)
 );
+
+server.on("clientError", (err, socket) => {
+  console.error(err);
+  socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
+});
