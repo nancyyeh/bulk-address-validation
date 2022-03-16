acbd31f0e7819a23c071a8338023ec027b0ac68c
Nancy Yeh <nancy@learnwithjuni.com>
2022-03-16 02:30:50 -0700

    path

--- routes/validator.js
+++ routes/validator.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const Address = require("../models/address");
 const { body, validationResult } = require("express-validator");
 const {
   stringifyAddress,
