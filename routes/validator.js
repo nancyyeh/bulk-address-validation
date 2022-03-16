af0e83d4f0f152024ef785ac9ac4d68659fbac98
Nancy Yeh <nancy@learnwithjuni.com>
2022-03-16 00:00:40 -0700

    added test

--- routes/validator.js
+++ routes/validator.js
@@ -17,16 +17,108 @@ let client = clientBuilder.buildUsStreetApiClient();
 const stringifyAddress = ({ line1, line2 = "", city, state, zip }) =>
   `${line1}${line2 ? ` ${line2}` : ""}, ${city}, ${state} ${zip}`;
 
+async function createSmartyBatch(addresses) {
+  const batch = new SmartyCore.Batch();
+  for (const address of addresses) {
+    const { address_line_one, city, state, zip_code } = address;
+
+    const addressString = stringifyAddress({
+      line1: address_line_one,
+      city,
+      state,
+      zip: zip_code,
+    });
+
+    const addressInDB = await Address.findOne({
+      input: addressString,
+    }).exec();
+
+    if (!addressInDB) {
+      let lookup = new Lookup();
+      lookup.street = address_line_one;
+      lookup.city = city;
+      lookup.state = state;
+      lookup.zipCode = zip_code;
+      batch.add(lookup);
+    }
+  }
+
+  return batch;
+}
+
+async function getSmartyAddresses(batch) {
+  if (batch.isEmpty()) {
+    return [];
+  }
+
+  const result = await client.send(batch);
+  const batchAddressData = result.lookups.map((lookup) => {
+    let addressData = undefined;
+    if (lookup.result && lookup.result.length) {
+      const lookupResult = lookup.result[0];
+      addressData = {
+        address_line_one: lookupResult.deliveryLine1,
+        city: lookupResult.components.cityName,
+        state: lookupResult.components.state,
+        zip_code: `${lookupResult.components.zipCode}-${lookupResult.components.plus4Code}`,
+        latitude: lookupResult.metadata.latitude.toString(),
+        longitude: lookupResult.metadata.longitude.toString(),
+        input: stringifyAddress({
+          line1: lookup.street,
+          city: lookup.city,
+          state: lookup.state,
+          zip: lookup.zipCode,
+        }),
+      };
+    }
+    return addressData;
+  });
+  return batchAddressData;
+}
+
+async function addAddressesToDb(addresses) {
+  for (const address of addresses) {
+    if (address) {
+      const {
+        address_line_one: line1,
+        city,
+        state,
+        zip_code: zip,
+        latitude,
+        longitude,
+        input,
+      } = address;
+      await Address.create({
+        line1,
+        city,
+        state,
+        zip,
+        latitude,
+        longitude,
+        input,
+      });
+    }
+  }
+}
+
 router.post(
   "/",
   [
     body().isArray().notEmpty(),
-    body("*.address_line_one", "address_line_one is required, one must be a string.")
+    body(
+      "*.address_line_one",
+      "address_line_one is required, one must be a string."
+    )
       .exists()
       .isString(),
     body("*.city", "city is required, must be a string.").exists().isString(),
-    body("*.state", "state is required, and must be a string.").exists().isString(),
-    body("*.zip_code", "zip code is required, and must be a string and 5 characters.")
+    body("*.state", "state is required, and must be a string.")
+      .exists()
+      .isString(),
+    body(
+      "*.zip_code",
+      "zip code is required, and must be a string and 5 characters."
+    )
       .exists()
       .isString()
       .isLength({ min: 5, max: 5 }),
@@ -34,107 +126,43 @@ router.post(
   async (req, res) => {
     const requestData = req.body;
     // Validate & take care of edge cases
-    if(!requestData){
-      return res.status(204).json({ message: "There was no content sent"})
+    if (!requestData) {
+      return res.status(204).json({ message: "There was no content sent" });
     }
-    if(requestData && requestData.length > 5){
-      return res.status(400).json({ errors: 'You can only validate a max of 5 addresses at a time.'})
+    if (requestData && requestData.length > 5) {
+      return res.status(400).json({
+        errors: "You can only validate a max of 5 addresses at a time.",
+      });
     }
     // Validate to ensure input is array and fit the requirements
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
-      return res.status(400).json({ message:"There is a problem with the input data format, please check on it", errors: errors.array() });
+      return res.status(400).json({
+        message:
+          "There is a problem with the input data format, please check on it",
+        errors: errors.array(),
+      });
     }
-
 
     // Find those request data that's not in the db, and create the batch job of input address that's not in the db
-    let batch = new SmartyCore.Batch();
-    for (const address of requestData) {
-      const { address_line_one, city, state, zip_code } = address;
-      let lookup = new Lookup();
-      lookup.street = address_line_one;
-      lookup.city = city;
-      lookup.state = state;
-      lookup.zipCode = zip_code;
-      const addressString = stringifyAddress({
-        line1: address_line_one,
-        city,
-        state,
-        zip: zip_code,
-      });
-      const addressInDB = await Address.findOne({
-        input: addressString,
-      }).exec();
-      if (!addressInDB) {
-        batch.add(lookup);
-      }
-    }
+    const batch = await createSmartyBatch(requestData);
 
     // If there is address that is not in the DB, valid it in Smarty
     let batchAddressData;
-    if (!batch.isEmpty()) {
-      try {
-        const result = await client.send(batch);
-        batchAddressData = result.lookups.map((lookup) => {
-          let addressData = undefined;
-          if (lookup.result && lookup.result.length) {
-            const lookupResult = lookup.result[0];
-            addressData = {
-              address_line_one: lookupResult.deliveryLine1,
-              city: lookupResult.components.cityName,
-              state: lookupResult.components.state,
-              zip_code: `${lookupResult.components.zipCode}-${lookupResult.components.plus4Code}`,
-              latitude: lookupResult.metadata.latitude.toString(),
-              longitude: lookupResult.metadata.longitude.toString(),
-              input: stringifyAddress({
-                line1: lookup.street,
-                city: lookup.city,
-                state: lookup.state,
-                zip: lookup.zipCode,
-              }),
-            };
-          }
-          return addressData;
-        });
-      } catch (err) {
-        console.log(`Failed to validate address(es) with Smarty: ${err}`);
-      }
+    try {
+      batchAddressData = await getSmartyAddresses(batch);
+    } catch (err) {
+      return res.status(500).json({
+        success: false,
+        message: "Fail to validate addresses with Smarty",
+      });
     }
 
-    // Create address that is not in DB
-    if (batchAddressData && batchAddressData.length) {
-      for (const address of batchAddressData) {
-        if (address) {
-          const {
-            address_line_one: line1,
-            city,
-            state,
-            zip_code: zip,
-            latitude,
-            longitude,
-            input,
-          } = address;
-          try {
-            const createData = await Address.create({
-              line1,
-              city,
-              state,
-              zip,
-              latitude,
-              longitude,
-              input,
-            });
-            console.log(createData);
-          } catch (err) {
-            console.log(`Failed to create address in db: ${err}`);
-          }
-        }
-      }
-    }
+    await addAddressesToDb(batchAddressData);
 
     // return data in the output format
-    let data = [];
-    for(const address of requestData){
+    const data = [];
+    for (const address of requestData) {
       const { address_line_one, city, state, zip_code } = address;
       const addressString = stringifyAddress({
         line1: address_line_one,
@@ -142,27 +170,22 @@ router.post(
         state,
         zip: zip_code,
       });
-      let addressDatainDB;
-      try {
-        addressDatainDB = await Address.findOne({
-          input: addressString,
-        }).exec();
-        if(addressDatainDB) {
-          data.push({              
-            address_line_one: addressDatainDB.line1,
-            city: addressDatainDB.city,
-            state: addressDatainDB.state,
-            zip_code: addressDatainDB.zip,
-            latitude: addressDatainDB.latitude,
-            longitude: addressDatainDB.longitude,
-            response: 'Valid Address'
-          })
-        } else {
-          data.push({ response: 'Invalid Address' })
-        }
-      } catch (err) {
-        console.log("Something wrong with finding address in DB!");
-        data.push({ response:'There was an error with process this address, please try again.'})
+      const addressDatainDB = await Address.findOne({
+        input: addressString,
+      }).exec();
+
+      if (addressDatainDB) {
+        data.push({
+          address_line_one: addressDatainDB.line1,
+          city: addressDatainDB.city,
+          state: addressDatainDB.state,
+          zip_code: addressDatainDB.zip,
+          latitude: addressDatainDB.latitude,
+          longitude: addressDatainDB.longitude,
+          response: "Valid Address",
+        });
+      } else {
+        data.push({ response: "Invalid Address" });
       }
     }
 
--- server.js
+++ server.js
@@ -1,26 +1,4 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-require('dotenv/config');
-
-app.use(express.json());
-
-//Connect to DB
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
-db.on('error', (error) =>  console.error('***ERROR***', error));
-db.once('open', () => console.log('Connected to Database'));
-
-//Import Routes
-const usAddress = require('./routes/validator');
-
-//Middlewares
-app.use('/validate', usAddress);
-
-//Routes
-app.get('/', (req, res) => {
-    res.send("This is Nancy's Bulk Address Validator Tool");
-});
+const app = require('./app');
 
 // Started Server
 const port = process.env.PORT || 3000;
