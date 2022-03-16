609da7893692e02c2489976806c859888fb8a63b
Nancy Yeh <nancy@learnwithjuni.com>
2022-03-16 02:09:08 -0700

    revert server start

--- server.js
+++ server.js
@@ -2,11 +2,6 @@ const app = require("./app");
 
 // Started Server
 const port = process.env.PORT || 3000;
-let server = app.listen(port, () =>
+app.listen(port, () =>
   console.log(`You have been connected to the port number:${port}`)
 );
-
-server.on("clientError", (err, socket) => {
-  console.error(err);
-  socket.end("HTTP/1.1 400 Bad Request\r\n\r\n");
-});
